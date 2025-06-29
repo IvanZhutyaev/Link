d27aab2c0327890eda5403904549cffbbb151dd8
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-06-29 07:53:42 +0300

    Add 3d-tour

--- backend/add_avaline_url.py
+++
@@ -1,7 +0,0 @@
-from Database.DB_connection import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    conn.execute(text("ALTER TABLE residential_complexes ADD COLUMN avaline_url VARCHAR;"))
-    conn.commit()
-    print("Столбец avaline_url успешно добавлен!") 
